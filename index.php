a257be08a59a45e290e0b2039f932026a0899495
Olga Khlud <khlud.olga@gmail.com>
2021-10-04 23:15:42 +0300

    add guzzle

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-<?php echo 'start project';
-      echo 'new branch';
+<?php
+
+require "vendor/autoload.php";
+
+$guzzle = new \GuzzleHttp\Client();
+$content = $guzzle->request('get', 'https://onlineitea.com.ua/course/php-advanced/');
+
+echo $content->getBody()->getContents();
+
